aafba0980968a9fa87955cc330af93ba4936d8e8
jorge.alberto <jorge.alberto@wilcompute.com>
2021-09-18 13:13:08 -0300

    add validator libraries

--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,31 @@ import {
   SwaggerDocumentOptions,
   SwaggerModule,
 } from '@nestjs/swagger';
-import { INestApplication } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
-  setupSwagger(app);
+  setupSwagger();
 
   await app.listen(process.env.PORT || 3000);
+
+  function setupSwagger(): void {
+    const config = new DocumentBuilder()
+      .setTitle('Prontuário Médico Eletronico')
+      .setDescription(
+        'Um sistema de prontuário eletrônico onde o médico pode cadastrar as informações do paciente e fazer os registros das consultas realizadas por paciente.',
+      )
+      .setVersion('1.0')
+      .build();
+    const options: SwaggerDocumentOptions = {
+      operationIdFactory: (controllerKey: string, methodKey: string) =>
+        methodKey,
+    };
+    const document = SwaggerModule.createDocument(app, config, options);
+
+    SwaggerModule.setup('api', app, document);
+  }
 }
 bootstrap();
-
-function setupSwagger(app: INestApplication): void {
-  const config = new DocumentBuilder()
-    .setTitle('Prontuário Médico Eletronico')
-    .setDescription(
-      'Um sistema de prontuário eletrônico onde o médico pode cadastrar as informações do paciente e fazer os registros das consultas realizadas por paciente.',
-    )
-    .setVersion('1.0')
-    .build();
-  const options: SwaggerDocumentOptions = {
-    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
-  };
-  const document = SwaggerModule.createDocument(app, config, options);
-
-  SwaggerModule.setup('api', app, document);
-}
